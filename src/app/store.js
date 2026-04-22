065a2195c93f3a3806d5d42b1c502de31f38e1ef
Randa-Khaled <320210601121@gmail.com>
2026-04-22 14:44:00 +0300

    persisting state implemented

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counterSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from "redux-persist/lib/storage/session";
 
-export const store = configureStore ({
-    reducer : {
-        counter : counterReducer,
+const persistConfig = {
+    key : "root", //the name used to store our data in the session storage
+    storage : storageSession, //the storage engine we use
+};
+
+/* wrapping the original counterReducer with persistence, so now when the
+state changes it automatically serializes and saves it to session storage,
+and on app startup it reads from session storage and restores the previous state (rehydration)*/
+const persistedReducer = persistReducer(persistConfig, counterReducer);
+
+export const store = configureStore({
+    reducer: {
+        counter: persistedReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            /* RTK comes with several built-in middleware enabled by default
+            this function inspects every action that gets dispatched and warns
+            in console if it finds something non-serializable ( can be converted to JSON )*/
+            serializableCheck: {
+                //if we do not ignore them, RTK will make so much warnings about them in the console because of the safety check
+                ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+            },
+        }),
 });
+
+export const persistor = persistStore(store);
--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,19 @@ import ReactDom from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import { store } from './app/store';
+import { store, persistor } from './app/store';
+/*component that delays rendering the app until saved state
+has been fully loaded from session storage into redux,
+which waits till the rehydration is done*/
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
